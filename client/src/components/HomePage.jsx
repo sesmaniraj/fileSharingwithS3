a4214e7b3d04637f980c6eaa8aba6028b7be9490
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-03 18:16:35 +0545

    Basic style done for homepage

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -2,22 +2,43 @@ import React, { useRef } from "react";
 
 const HomePage = () => {
   const inputUserRef = useRef();
+
   const handleUploadClick = () => {
     inputUserRef.current.click();
   };
+
   return (
-    <div>
-      <h1>FileSharing</h1>
-      <p>Convinent way to share file in 3 easy step</p>
-      <span>1</span>
-      <input type="file" ref={inputUserRef} className="hidden" />
-      <button className="bg-blue-400" onClick={handleUploadClick}>
-        Select to upload file
-      </button>
-      <span>2</span>
-      <p>wait until file is upload</p>
-      <span>3</span>
-      <p>the file will be available at :http://localhost:8000</p>
+    <div className="container mx-auto p-8">
+      <h1 className="text-4xl font-bold mb-4">FileSharing</h1>
+      <p className="text-lg mb-6">
+        Convenient way to share files in 3 easy steps
+      </p>
+
+      <div className="flex items-center mb-4">
+        <span className="text-2xl  mr-2">1</span>
+        <input type="file" ref={inputUserRef} className="hidden" />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleUploadClick}
+        >
+          Select to upload file
+        </button>
+      </div>
+
+      <div className="mb-4">
+        <span className="text-2xl  mr-2">2</span>
+        <p>Wait until the file is uploaded</p>
+      </div>
+
+      <div className="mb-4">
+        <span className="text-2xl  mr-2">3</span>
+        <p>
+          The file will be available at:{" "}
+          <a href="http://localhost:8000" className="text-blue-500">
+            http://localhost:8000
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
